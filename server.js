11276cbfde2bdf6cb3de13f5ebda61bbc1d4693c
akondshaheen <akondshaheen.es@gmail.com>
2020-07-12 23:03:21 +0200

    Cms Complete

---
+++ server.js
@@ -0,0 +1,48 @@
+var fs = require("fs");
+var express = require("express");
+var formidable = require("express-formidable");
+
+var app = express();
+
+app.use(express.static("public"));
+app.use(formidable());
+
+app.post("/create-post", function (req, res) {
+  fs.readFile(__dirname + "/data/posts.json", (err, data) => {
+    if (err) throw err;
+    var info = JSON.parse(data);
+
+    var mainInfo = {
+      ...info,
+      [Date.now()]: req.fields.blogpost,
+    };
+    var lastInfo = JSON.stringify(mainInfo);
+
+    console.log("data", info);
+    fs.writeFile(__dirname + "/data/posts.json", lastInfo, (err) => {
+      if (err) throw err;
+      console.log("Data written to file");
+    });
+  });
+
+  res.send("Hello there!");
+});
+
+app.get("/get-posts", function (req, res) {
+  res.sendfile(__dirname + "/data/posts.json");
+});
+
+// fs.writeFile(__dirname + "/data/posts.json", yourData, function (error) {
+//   // do something
+// });
+
+// fs.readFile(__dirname + "/data/posts.json", "utf8", function (error, file) {
+//   // console.log(file.toString());
+//   var parsedFile = JSON.parse(file);
+//   console.log(parsedFile);
+//   // fs.writeFile("posts.json", file);
+// });
+
+app.listen(8000, function () {
+  console.log("Server is listening on port 3000. Ready to accept requests!");
+});
